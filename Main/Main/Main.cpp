77ef5b5ba36059d791341969b99fe54abacc0090
Arlodactyl <102116019+Arlodactyl@users.noreply.github.com>
2025-06-10 10:35:54 +1200

    Update Main.cpp
    Made more readable - added borders - have tested.

--- Main/Main/Main.cpp
+++ Main/Main/Main.cpp
@@ -10,18 +10,16 @@ using namespace std;
 
 struct User
 {
-protected: // Private Variables
+protected:
     string username;
     string password;
 
-public: // Public Variables
+public:
     string name;
     string surname;
     int age;
 
-    // Cart here...?
-
-    User() // Default constructor to initialize values
+    User()
     {
         username = " ";
         password = " ";
@@ -30,13 +28,12 @@ public: // Public Variables
         age = 0;
     }
 
-    void setUpPassword() // Function to set Username and Passowrd 
+    void setUpPassword()
     {
         cout << "Enter A Password : ";
-        getline(cin, password); // Getting user input for Password
+        getline(cin, password);
         cout << endl;
 
-        // Error Handling (Password can't be the same as the username)
         while (password == username)
         {
             cout << "\n(Error Message) Please enter a password that doesn't match your Username!\n" << endl;
@@ -45,74 +42,70 @@ public: // Public Variables
             getline(cin, password);
             cout << endl;
         }
-
     }
 
-    void getInformation() // Funciton for getting the users information
+    void getInformation()
     {
-        // Getting Name of User
         cout << "Name : ";
         getline(cin, name);
         cout << endl;
-        // Getting Surname of User
+
         cout << "Surname : ";
         getline(cin, surname);
         cout << endl;
-        //Getting Age of User
+
         cout << "Age : ";
 
-        while (true) // auto while loop
+        while (true)
         {
-            cin >> age; // user input
+            cin >> age;
             cout << endl;
 
-            if (cin.fail() || age < 5 || age > 120) // if statement, checking for invalid input with .fail(), checking if age is less then 5 or greather then 120
+            if (cin.fail() || age < 5 || age > 120)
             {
-                cin.clear(); // clearing the invalid input value 
-                cin.ignore(numeric_limits<streamsize>::max(), '\n'); // ingoring the invalid input, which prevents infinite loops   
-                cout << "Invlaid input, please enter a valid number : "; // Error Message
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                cout << "Invalid input, please enter a valid number : ";
             }
             else
             {
-                break; // Valid inpit, existing loop
+                break;
             }
         }
-
     }
 
-    void setUsername(string& tempUsername) // Function for setting the users username 
+    void setUsername(string& tempUsername)
     {
         username = tempUsername;
     }
 
-    string getUsername() const // Function for returning a const value of users username 
+    string getUsername() const
     {
         return username;
     }
 
-    bool checkPassword(string& tempPassword) // Function for returning a bool value to check if the password is correct or not
+    bool checkPassword(string& tempPassword)
     {
         return tempPassword == password;
     }
 
-    void saveToFile(ofstream& file) const // Function for saving the information in the file
+    void saveToFile(ofstream& file) const
     {
         file << username << ',' << password << ',' << name << ',' << surname << ',' << age << endl;
     }
 
-    bool loadFromLine(string& line) // Function for loading users data from a file
+    bool loadFromLine(string& line)
     {
-        stringstream ss(line); // Stringstream to break the line into parts
-        string username, password, name, surname, stringAge; // Temp variables
+        stringstream ss(line);
+        string username, password, name, surname, stringAge;
 
-        getline(ss, username, ','); // Extracting Username
-        getline(ss, password, ','); // Extracting Password
-        getline(ss, name, ','); // Extracting Name
-        getline(ss, surname, ','); // Extracting Surname
-        getline(ss, stringAge); // Extracting Age (Still in a string)
-        int age = stoi(stringAge); // stoi (String to integer), this function converts a string into a integer
+        getline(ss, username, ',');
+        getline(ss, password, ',');
+        getline(ss, name, ',');
+        getline(ss, surname, ',');
+        getline(ss, stringAge);
+        int age = stoi(stringAge);
 
-        // Assigning the values to the member variable of the user
         this->username = username;
         this->password = password;
         this->name = name;
@@ -121,73 +114,90 @@ public: // Public Variables
 
         return true;
     }
-
 };
 
+// ─────────────────────────────────────────────
+// UI Border Helpers
+// ─────────────────────────────────────────────
+void printStrongBorder(const string& title)
+{
+    int width = title.length() + 10;
+    cout << string(width, '#') << endl;
+    cout << "##  " << title << string(width - 6 - title.length(), ' ') << "##" << endl;
+    cout << string(width, '#') << endl;
+}
+
+void printMenuOption(const string& option)
+{
+    cout << "|| " << option << endl;
+}
+
+// ─────────────────────────────────────────────
+// Function Declarations
+// ─────────────────────────────────────────────
 void saveAllUsers(const vector<User>& users);
 void loadAllUsers(vector<User>& users);
-
 bool usernameExists(const vector<User>& users, const string& username);
+void mainMenu(vector<User>& users);
+void adminMenu(vector<User>& users);
+void userLogin(vector<User>& users);
 
-void mainMenu(vector<User>& users); // Main Menu Function
-void adminMenu(vector<User>& users); // Admin Menu
-void userLogin(vector<User>& users); // User Login Menu
-
+// ─────────────────────────────────────────────
+// Main Function
+// ─────────────────────────────────────────────
 int main()
 {
-    vector <User> users; // Storing User inside a vector
-    loadAllUsers(users); // Function for loading all users from the file
-
-    mainMenu(users); // Main Menu Function
-
-    saveAllUsers(users); // Function for saving all users to the file
+    vector <User> users;
+    loadAllUsers(users);
+    mainMenu(users);
+    saveAllUsers(users);
     return 0;
 }
 
-void saveAllUsers(const vector<User>& users) // Function for saving all users inside the file
+// ─────────────────────────────────────────────
+void saveAllUsers(const vector<User>& users)
 {
-    ofstream file("Users.txt"); // Output File 
-    if (file.is_open()) // Checking if the file has opened
+    ofstream file("Users.txt");
+    if (file.is_open())
     {
-        for (const auto& user : users) // Using a auto for loop to save all the data in the user to the file
+        for (const auto& user : users)
         {
             user.saveToFile(file);
         }
-
-        file.close(); // Closing the file
+        file.close();
     }
 }
 
-void loadAllUsers(vector<User>& users) // Funciton for loading all the users information from the file
+void loadAllUsers(vector<User>& users)
 {
-    ifstream file("Users.txt"); // Input File 
+    ifstream file("Users.txt");
+    if (file.is_open())
     {
-        if (file.is_open()) // Checking if the file has opened
+        string line;
+        while (getline(file, line))
         {
-            string line; // Using a string variales to gather the information
-            while (getline(file, line))
+            User user;
+            if (user.loadFromLine(line))
             {
-                User user;
-                if (user.loadFromLine(line)) // Checking if sussceful in getting the information
-                {
-                    users.push_back(user); // Pushing back the users information into the vector
-                }
+                users.push_back(user);
             }
         }
-        file.close(); // Closing the file
     }
+    file.close();
 }
 
+// ─────────────────────────────────────────────
 void mainMenu(vector<User>& users)
 {
     char option;
-    cout << "\t[Aotearoa Inventory System]\n" << endl;
+    printStrongBorder("Aotearoa Inventory System");
 
     do
     {
-        cout << "1. Store" << endl;
-        cout << "2. Login" << endl;
-        cout << "3. Exit" << endl;
+        printMenuOption("1. Store");
+        printMenuOption("2. Login");
+        printMenuOption("3. Exit");
+        cout << string(30, '=') << endl;
 
         cout << "Option : ";
         cin >> option;
@@ -195,210 +205,179 @@ void mainMenu(vector<User>& users)
 
         switch (option)
         {
-        case '1': // Case 1 goes to store
-        {
-            // Store menu;
+        case '1':
             cout << "Store Menu..." << endl;
             break;
-        }
-        case '2': // Case 2 goes to login menu
-        {
-            // Login Menu;
+        case '2':
             userLogin(users);
             break;
-        }
-        case '3': // Case 3 exits program
-        {
-            // Exit
+        case '3':
             cout << "Exiting Program!" << endl;
             break;
-        }
-        case '&': // Special character to get access to Admin Menu (Supplied in the read me file)
-        {
+        case '&':
             system("cls");
-            // Admin login menu
             adminMenu(users);
             break;
-        }
         default:
             system("cls");
             cout << "\n(Error Message) Please enter the correct input\n" << endl;
         }
     } while (option != '3');
-
 }
 
+// ─────────────────────────────────────────────
 void adminMenu(vector<User>& users)
 {
-    string userAdmin = "ADMIN"; // Default varables
-    string passAdmin = "PASSWORD"; // Default varables
+    string userAdmin = "ADMIN";
+    string passAdmin = "PASSWORD";
 
     char goBack;
     string username;
     string password;
 
-    cout << "\t[Admin Login]\n" << endl;
+    printStrongBorder("Admin Login");
 
-    do // Do While loop
+    do
     {
-        cout << "1. Login" << endl;
-        cout << "2. Back" << endl;
-        cout << "Option : ";
+        printMenuOption("1. Login");
+        printMenuOption("2. Back");
+        cout << string(30, '=') << endl;
 
-        cin >> goBack; // Getting user input
-        cin.ignore(); // ingoring the cin operator
+        cout << "Option : ";
+        cin >> goBack;
+        cin.ignore();
         cout << endl;
 
-        switch (goBack) // Switch Case
+        switch (goBack)
         {
-        case '1': // Login Menu
-        {
+        case '1':
             cout << "Username : ";
-            getline(cin, username); // Getting user input 
+            getline(cin, username);
 
             cout << "Password : ";
-            getline(cin, password); // Getting user input
+            getline(cin, password);
 
-            if (username != userAdmin || password != passAdmin) // Check if user input is Correct or Not
+            if (username != userAdmin || password != passAdmin)
             {
-                system("cls"); // clearing screen for clear display
-                cout << "\n(Error Message) Username or Password incorrected!\n" << endl;
+                system("cls");
+                cout << "\n(Error Message) Username or Password incorrect!\n" << endl;
             }
             else
             {
                 cout << "Admin Panel..." << endl;
-                // Call Admin Panel...
             }
             break;
-        }
-        case '2': // Exiting Switch case
-        {
+        case '2':
             break;
-        }
         default:
-            system("cls"); // Clearing screen for clear display
-            cout << "\n(Error Message) Please enter the correct input\n" << endl; // Givng user an error message when wrong
+            system("cls");
+            cout << "\n(Error Message) Please enter the correct input\n" << endl;
         }
-
-
-
-
-    } while (goBack != '2'); // if goBack == 2 then close the while loop
-
+    } while (goBack != '2');
 }
 
-bool usernameExists(const vector<User>& users, const string& username) // Function for checking if username exists
+// ─────────────────────────────────────────────
+bool usernameExists(const vector<User>& users, const string& username)
 {
-    for (const auto& user : users) // Auto for loop for users
+    for (const auto& user : users)
     {
-        if (user.getUsername() == username) // Checking if the temperary username is the same with any other username in the system
+        if (user.getUsername() == username)
         {
-            return true; // Returning true if its found one
+            return true;
         }
     }
-
-    return false; // Returning false if not
+    return false;
 }
 
-void userLogin(vector<User>& users) // User Login Menu
+// ─────────────────────────────────────────────
+void userLogin(vector<User>& users)
 {
     char option;
 
-    do // Do while loop 
+    do
     {
-        cout << "\t[User Login]\n" << endl;
+        printStrongBorder("User Login");
 
-        cout << "1. Sign In" << endl;
-        cout << "2. Register An Account" << endl;
-        cout << "3. Back" << endl;
+        printMenuOption("1. Sign In");
+        printMenuOption("2. Register An Account");
+        printMenuOption("3. Back");
+        cout << string(35, '=') << endl;
 
         cout << "Option : ";
-        cin >> option; // Getting user's input
+        cin >> option;
         cout << endl;
 
         switch (option)
         {
-        case '1': // Sign in Case
+        case '1':
         {
             string tempUsername, tempPassword;
             bool userFound = false;
 
-            cin.ignore(); // Ignoring cin input
+            cin.ignore();
             cout << "Username : ";
-            getline(cin, tempUsername); // Getting users input for username
+            getline(cin, tempUsername);
             cout << endl;
 
             cout << "Password : ";
-            getline(cin, tempPassword); // Getting users input for password
+            getline(cin, tempPassword);
             cout << endl;
 
-            for (User& user : users) // Auto for loop to read all users data
+            for (User& user : users)
             {
-                if (user.getUsername() == tempUsername && user.checkPassword(tempPassword)) // Checking if users has entered the same values for a user
+                if (user.getUsername() == tempUsername && user.checkPassword(tempPassword))
                 {
-                    cout << "Welcome back, " << user.name << endl; // Giving the user a welcome back message if loged in
-                    userFound = true; // User found is set to true
-
-
-                    // Taking them to the store......
-                    break; // Breaking loop
+                    cout << "Welcome back, " << user.name << endl;
+                    userFound = true;
+                    break;
                 }
             }
 
-            if (!userFound) // If User hasn't been found then give error message
+            if (!userFound)
             {
-                cout << "\n(Error Message) Invalid username or password!\n" << endl; // Error message
+                cout << "\n(Error Message) Invalid username or password!\n" << endl;
             }
 
-            break; // Breaking switch case
-
+            break;
         }
-        case '2': // Sign up case
+        case '2':
         {
-            cin.ignore(); // Ignoring cin input
-
-            User newUser; // User class
-            string username; // Temperary username variable
+            cin.ignore();
+            User newUser;
+            string username;
             bool exists;
 
-            do // Do while loop
+            do
             {
                 cout << "Enter A Username : ";
-                getline(cin, username); // Geting user input for Username
+                getline(cin, username);
                 cout << endl;
 
-                exists = usernameExists(users, username); // Checking for the same usernames, if so
+                exists = usernameExists(users, username);
 
                 if (exists)
                 {
-                    cout << "\n(Error Message) That username is already taken. Please choose another!\n" << endl; // Error message
+                    cout << "\n(Error Message) That username is already taken. Please choose another!\n" << endl;
                 }
                 else
                 {
-                    break; // Breaking out of while loop
+                    break;
                 }
-            } while (true); // True by default until broken or false
+            } while (true);
 
-            newUser.setUsername(username); // Setting the usernames into the users member value
-            newUser.setUpPassword(); // Setting the users password, which is stored in the users member value
-            newUser.getInformation(); // Getting the users information and storing into the users member values
-            users.push_back(newUser); // Pushing back into the vector switch for storing
+            newUser.setUsername(username);
+            newUser.setUpPassword();
+            newUser.getInformation();
+            users.push_back(newUser);
 
-            cout << "Registation Completed!" << endl; // Displaying a message to let the user know they created an account
-            break; // Breaking out of switch case
+            cout << "Registration Completed!" << endl;
+            break;
         }
-        case '3': // Case 3 - Go back
-        {
-            // Going back to main menu
-            break; // Breaking out of case
-        }
+        case '3':
+            break;
         default:
-
-            system("cls"); // Clearing screen for clear display
-            cout << "\n(Error Message) Please enter the correct input\n" << endl; // Givng user an error message when wrong
+            system("cls");
+            cout << "\n(Error Message) Please enter the correct input\n" << endl;
         }
-    } while (option != '3'); // Keep loop while option doesn't equal 3 (3. Go back)
-
+    } while (option != '3');
 }
-
-
